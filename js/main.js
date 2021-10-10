0e1b43b05cc40d54331efd468b388656b6661b0d
SRE-Adolfo.MX <adolfoing15@hotmail.com>
2021-10-10 01:16:51 -0500

    Add index-create-post Devto

---
+++ js/main.js
@@ -0,0 +1,27 @@
+console.log("js")
+
+
+
+ $('.chkd').click(function() {
+    window.location.href='newPost.html';
+    return false;
+});
+
+
+
+$( '.Publish' ).on( 'click', function() {
+
+ 
+    let addCoverImage = $('#Add').val();
+    let uploadImage = $('#Upload').val();
+    let postTitle = $('#article-form-title').val();
+    let postContent = $('#article_body_markdown').val();
+    let objData = {
+        addCoverImage,
+        uploadImage,
+        postTitle,
+        postContent
+    }
+console.log(objData)
+//createProduct(arrayInfo)
+});
