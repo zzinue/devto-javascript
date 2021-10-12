5927c66dc9ed719647b8d1d45e301a06b90bf960
SRE-Adolfo.MX <adolfoing15@hotmail.com>
2021-10-11 21:46:50 -0500

    Dinamyc medium post in index.html

--- js/main.js
+++ js/main.js
@@ -50,6 +50,7 @@ const createPost = (dataObj) => {
 }
 
 
+
 const getData = () => {
     let dataPost
     $.ajax({
@@ -79,9 +80,71 @@ const getData = () => {
 }
 
 
-
 const printPost = arrayPots => {
     let list = document.querySelector(".auto_card")
+    let firstList = document.querySelector(".firstAuto_card")
+    let firstPost = [];
+    firstPost.push(arrayPots[0]);
+    
+    let allPostFirst = firstPost.reduce((acc, posts) => {
+        let {id,
+            addCoverImage,
+            uploadImage,
+            postTitle,
+            postContent,
+            user,
+            today} = posts
+
+        let cardPostFirst = `
+        <article class="card_posts">
+        <div>
+            <img  class="card_img" src=${addCoverImage} style="background-color:#dddddd;" class="crayons-article__cover__image" alt="Cover image for FullStack - How to create a working blogging website with pure HTML, CSS and JS in 2021." >
+        </div>
+        <div class="perfil">
+            <div>
+                <img class="img_post" src=${uploadImage} alt="">
+            </div>
+            <div>
+            <h4>${user}<h4>    
+            <h4> ${today}</h4>  
+            </div>
+        </div>    
+    
+        <div class="post">
+            <p>
+               ${postTitle}
+            </p>
+        </div>
+    
+        <div class="hashtags">
+            <span> #tech</span>
+            <span> #it</span>
+            <span>#kodemia</span>
+            <span>#devteam</span>
+        </div>
+    
+        <div class="reactions">
+            <div>
+              <span> 
+                <svg class="crayons-icon" width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M18.884 12.595l.01.011L12 19.5l-6.894-6.894.01-.01A4.875 4.875 0 0112 5.73a4.875 4.875 0 016.884 6.865zM6.431 7.037a3.375 3.375 0 000 4.773L12 17.38l5.569-5.569a3.375 3.375 0 10-4.773-4.773L9.613 10.22l-1.06-1.062 2.371-2.372a3.375 3.375 0 00-4.492.25v.001z"></path></svg> 
+                  2 reactions</span> 
+              <span> 
+                <svg class="crayons-icon" width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M10.5 5h3a6 6 0 110 12v2.625c-3.75-1.5-9-3.75-9-8.625a6 6 0 016-6zM12 15.5h1.5a4.501 4.501 0 001.722-8.657A4.5 4.5 0 0013.5 6.5h-3A4.5 4.5 0 006 11c0 2.707 1.846 4.475 6 6.36V15.5z"></path></svg>      
+            </span> 
+            </div>
+        <div> 4 min read 
+            <button>Save</button>
+            <button data-product-id="${id}" value="${id}" class="Del btn btn-danger">Eliminar <i class="fas fa-cart-plus"></i></button>
+            </div>
+                
+        </div>
+    </article>
+        `
+        return acc + cardPostFirst
+    }, "")
+    firstList.innerHTML = allPostFirst
+
+    arrayPots.shift();
 
     let allPost = arrayPots.reduce((acc, posts) => {
         let {id,
@@ -96,7 +159,7 @@ const printPost = arrayPots => {
         <article class="card_posts">
         <div class="perfil">
             <div>
-                <img class="img_post" src="https://res.cloudinary.com/practicaldev/image/fetch/s--EKbJ_abL--/c_fill,f_auto,fl_progressive,h_90,q_auto,w_90/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/685854/9a72b11f-bec3-4ff9-b199-e7c6a62c41f9.jpeg" alt="">
+                <img class="img_post" src=${uploadImage} alt="">
             </div>
             <div>
             <h4>${user}<h4>    
